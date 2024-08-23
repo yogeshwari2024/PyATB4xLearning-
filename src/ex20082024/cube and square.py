3d1bcc8ef4052cc07ae435eb16b8e45deada6d62
Repo_demo <yogeshwaribhagirath11@gmail.com>
2024-08-23 23:03:03 +0530

    cube and square py program added

---
+++ src/ex20082024/cube and square.py
@@ -0,0 +1,8 @@
+### Task #6
+
+# Develop a Python script that calculates the square and cube of a given number.
+
+num = int(input("enter the number : "))
+
+print("the square of given number is : ", num**2)
+print("the cube of given number is :", num**3)
